5e1eff661bceadd270fbc06aa04ce280f36d6630
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-01 17:58:04 +0200

    Update image-finder (searchbar)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import Searchbar from "./Searchbar/Searchbar";
+
+class App extends React.Component {
+  state = {
+    searchWord: '',
+    images: [],
+  }
+
+  handleFormSubmit = searchWord => {
+    this.setState({ searchWord, images: [], });
+  }
+
+  render() {
+    return (
+      <div>
+        <ToastContainer autoClose={2000} hideProgressBar={true}/>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+      </div>
+    );
+ } 
+}
+
+export default App;
